6220f76a3c486fb85075297e09219d2611a2b6e9
tanimpersonal <tashintanim2019@gmail.com>
2024-03-06 19:40:39 +0600

    red-green-blue-cards

--- components/FlatCards/FlatCards.tsx
+++ components/FlatCards/FlatCards.tsx
@@ -4,9 +4,43 @@ import React from 'react';
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style={[styles.redContainer, styles.red]}>
+        <Text style={styles.text}>Red</Text>
+      </View>
+      <View style={[styles.redContainer, styles.green]}>
+        <Text style={styles.text}>Green</Text>
+      </View>
+      <View style={[styles.redContainer, styles.blue]}>
+        <Text style={styles.text}>Blue</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    textAlign: 'center',
+  },
+  redContainer: {
+    height: 100,
+    width: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    margin: 8,
+  },
+  red: {
+    backgroundColor: 'red',
+  },
+  green: {
+    backgroundColor: 'green',
+  },
+  blue: {
+    backgroundColor: 'blue',
+  },
+  text: {
+    color: 'white',
+  },
+});
